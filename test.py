c41e2779be673fccd62cedef1de88e9c7fdad857
Rowan Dash <rowan.dash@swansea.ac.uk>
2023-07-20 14:33:30 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
+"""This is a test function.
+
+    :param kind: Optional "kind" of ingredients.
+    :type kind: list[str] or None
+    :raise lumache.InvalidKindError: If the kind is invalid.
+    :return: The ingredients list.
+    :rtype: list[str]
+    """
 def test(kind=None):
     """This is a test function.
 
