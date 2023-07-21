2ae598df653e606963e35f89a52a2db86775a06c
Rowan Dash <rowan.dash@swansea.ac.uk>
2023-07-21 11:14:13 +0100

    Update autoapi paths

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ extensions = [
     'autoapi.extension',
 ]
 
-autoapi_dirs = ['../']
+autoapi_dirs = ['..']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
