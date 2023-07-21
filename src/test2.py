68e22dfe9bd1f65b2e5433b8604218a2e318f410
Rowan Dash <rowan.dash@swansea.ac.uk>
2023-07-21 14:27:36 +0100

    Update test2.py

--- src/test2.py
+++ src/test2.py
@@ -1,5 +1,5 @@
 def test(kind=None):
-    """This is a test function.
+    """This is a copy of the test function.
 
     :param kind: Optional "kind" of ingredients.
     :type kind: list[str] or None
